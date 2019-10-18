cfe480b99eb3556d35d402157f00cddee8d26533
Nik68rus <nik200x@mail.ru>
2019-10-18 18:38:42 +0300

    добавил пропсы

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app/app.jsx";
+import App from "./components/app/app";
 
 const init = () => {
+  const settings = {
+    gameTime: 5,
+    errorCount: 3,
+  };
+
   ReactDOM.render(
-      <App />,
+      <App
+        errorCount={settings.errorCount}
+        gameTime={settings.gameTime}
+      />,
       document.querySelector(`#root`)
   );
 };
