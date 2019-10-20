f4c6d15b2f0ee7f4bc454b1c39e0b1f959bda3fa
Nik68rus <nik200x@mail.ru>
2019-10-20 19:43:57 +0300

    написал первые тесты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,20 @@
-import WelcomeScreen from "../welcome-screen/welcome-screen";
+import React from 'react';
+import PropTypes from 'prop-types';
+import WelcomeScreen from '../welcome-screen/welcome-screen';
 
 const App = (props) => {
-  const {gameTime, errorCount} = props;
+  const {gameTime, errorCount, clickHandler} = props;
   return <WelcomeScreen
     time={gameTime}
     errorCount={errorCount}
+    clickHandler={clickHandler}
   />;
 };
 
 App.propTypes = {
   gameTime: PropTypes.number.isRequired,
   errorCount: PropTypes.number.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default App;
---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import WelcomeScreen from '../welcome-screen/welcome-screen';
+
+Enzyme.configure({adapter: new Adapter()});
+
+it(`Button click action called once`, () => {
+  const clickHandler = jest.fn();
+
+  const welcomeScreen = shallow(
+      <WelcomeScreen
+        time={0}
+        errorCount={0}
+        clickHandler={clickHandler}
+      />
+  );
+
+  const startBtn = welcomeScreen.find(`button`);
+  startBtn.simulate(`click`);
+  expect(clickHandler).toHaveBeenCalledTimes(1);
+});
